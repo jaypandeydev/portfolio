b205d6cbe38418b73dbb27e29bcf7932b28b0e30
Jay Pandey <jp.pandey@hotmail.com>
2024-06-10 16:02:26 +0530

    second commit with changes added Banner changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import Banner from "./components/banner/Banner";
+import Features from "./components/features/Features";
 import Main from "./components/main"
-import Nav from "./components/nav"
+import Navbar from "./components/navbar/Navbar";
+//import Nav from "./components/nav"
+
 
 function App() {  
 
   return (
-    <div className="App">
-      <Nav/>
+    <div className="w-full h-auto bg-gray-800 text-white px-4">
+      <Navbar/>
       <Main/>
+      <Banner />
+      <Features />
     </div>
   );
 }
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,19 +2,19 @@
 const main = () => {
   return (
     <div>      
-      <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left  sm:max-3xl:bg-blue-50 sm:max-2xl:p-4">
+      <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left  sm:max-2xl:p-4 border-b-[1px] border-b-gray-600">
             <div className="pt-8 pr-8 pl-8 pb-8 items-center justify-between">
               <div className="flex-col items-start gap-8">               
-                <p className="text-gray-900 text-xl font-bold leading-10 self-stretch" >Hey, I'm Jay</p>
+                <p className="text-cyan-50 text-xl font-bold leading-10 self-stretch" >Hey, I'm Jay</p>
                 <br />
-                <h1 className="text-gray-900 text-7xl font-bold leading-10 self-stretch"> 
+                <h1 className="text-cyan-50 text-7xl font-bold leading-10 self-stretch"> 
                   <span>Full Stack</span>
                   <br />
                   <br />
                   Developer
                   </h1>
                   <br />
-                  <p className="text-blue-950 text-2xl font-normal leading-9">
+                  <p className="text-cyan-50 text-2xl font-normal leading-9">
                   I am passionate full stack developers with a diverse range of expertise and a shared commitment to creating robust, scalable, and user-friendly applications. Our mission is to bridge the gap between innovative ideas and their realization through cutting-edge technology and seamless integration of both front-end and back-end solutions.
                   </p>                  
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
@@ -22,7 +22,7 @@ const main = () => {
                   </button>                              
               </div>
             </div>
-            <div className="w-full rounded-lg px-24">
+            <div className="w-full rounded-lg flex justify-center items-center">
             <img src="public/IMG_7871 copy 2.png" alt="Hero Section"></img>
             </div>
       </section>
